e77dcfbea4e77d611fd9ae598ebd5e3d4f82c763
Angela Kim <angela_kim7@hotmail.com>
2020-10-06 10:13:49 -0700

    routing in app.js, WarehouseEdit and WarehouseAdd components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Warehouse from '';
+import WarehouseDetails from '';
+import Inventory from '';
+import InventoryDetails from '';
 
-function App() {
+import WarehouseEdit from './WarehouseEdit';
+
+class App extends React.Component {
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Redirect exact from = '/' to = '/warehouse'/>
+          <Route exact = '/warehouse' component {Warehouse}/>
+          <Route exact path = '/warehouse/:id' component = {WarehouseDetails}/>
+          <Route exact = '/inventory' component = {Inventory}/>
+          <Route exact path = '/inventory/:id' component = {InventoryDetails}/>
+        </Switch>
+      </BrowserRouter>
+    </>
   );
+  }
 }
 
 export default App;
---
+++ client/src/WarehouseEdit/WarehouseEdit.jsx
@@ -0,0 +1,135 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+class WarehouseEdit extends React.Component {
+    render() {
+        return (
+            <>
+              <div className = 'header'>
+                //put in 'edit warehouse' part
+              </div>
+
+                <div className = 'form'>
+                  <form onSubmit = {this}>
+                    <div className = 'form__details'>
+                        <h2 className = 'form__details-heading'>Warehouse Details</h2>
+
+                        <div className = 'form__details-input'>
+
+                          <div className = 'form__details-input-section'>
+                            <label className = 'form__details-input-subheading'>Warehouse Name</label>
+                            <input
+                                type = 'text'
+                                id = {this}
+                                className = 'form__details-input-text'
+                                name = {this}
+                                value = {this}
+                            />
+                          </div>
+
+                          <div className = 'form__details-input-section'>
+                            <label className = 'form__details-input-subheading'>Street Address</label>
+                            <input
+                                type = 'text'
+                                id = {this}
+                                className = 'form__details-input-text'
+                                name = {this}
+                                value = {this}
+                            />
+                          </div>
+
+
+                          <div className = 'form__details-input-section'>
+                            <label className = 'form__details-input-subheading'>City</label>
+                            <input
+                                type = 'text'
+                                id = {this}
+                                className = 'form__details-input-text'
+                                name = {this}
+                                value = {this}
+                            />
+                          </div>
+
+
+                          <div className = 'form__details-input-section'>
+                            <label className = 'form__details-input-subheading'>Country</label>
+                            <input
+                                type = 'text'
+                                id = {this}
+                                className = 'form__details-input-text'
+                                name = {this}
+                                value = {this}
+                            />
+                          </div>
+                        </div>
+                    </div>
+
+
+
+                     <div className = 'form__details'>
+                        <h2 className = 'form__details-heading'>Contact Details</h2>
+
+                        <div className = 'form__details-input'>
+
+                          <div className = 'form__details-input-section'>
+                            <label className = 'form__details-input-subheading'>Contact Name</label>
+                            <input
+                                type = 'text'
+                                id = {this}
+                                className = 'form__details-input-text'
+                                name = {this}
+                                value = {this}
+                            />
+                          </div>
+
+                          <div className = 'form__details-input-section'>
+                            <label className = 'form__details-input-subheading'>Position</label>
+                            <input
+                                type = 'text'
+                                id = {this}
+                                className = 'form__details-input-text'
+                                name = {this}
+                                value = {this}
+                            />
+                          </div>
+
+
+                          <div className = 'form__details-input-section'>
+                            <label className = 'form__details-input-subheading'>Phone Number</label>
+                            <input
+                                type = 'text'
+                                id = {this}
+                                className = 'form__details-input-text'
+                                name = {this}
+                                value = {this}
+                            />
+                          </div>
+
+
+                          <div className = 'form__details-input-section'>
+                            <label className = 'form__details-input-subheading'>Email</label>
+                            <input
+                                type = 'email'
+                                id = {this}
+                                className = 'form__details-input-text'
+                                name = {this}
+                                value = {this}
+                            />
+                          </div>
+
+                        </div>
+                    </div>
+
+                    <div className = 'form__details-button'>
+                        <button>Cancel</button>
+                        <button>Save</button>
+                    </div>
+
+                  </form>
+                </div>
+            </>
+        )
+    }
+}
+
+export default WarehouseEdit;
