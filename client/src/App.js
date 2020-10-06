df7739cae92c71d5cd8beeb7c4c80f2dca99ecbb
Angela Kim <angela_kim7@hotmail.com>
2020-10-06 10:38:05 -0700

    resolved conflicts on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import Header from './components/Header/Header'
 import './App.css';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Warehouse from '';
+import WarehouseDetails from './Components/WareHouseDetails/WareHouseDetails';
+import Inventory from '';
+import InventoryDetails from '';
+import Header from './Components/Header/Header'
 
-function App() {
+class App extends React.Component {
+  render() {
   return (
-    <div className="App">
-     <Header />
-    </div>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Switch>
+          <Redirect exact from = '/' to = '/warehouse'/>
+          <Route exact = '/warehouse' component {Warehouse}/>
+          <Route exact path = '/warehouse/:id' component = {WarehouseDetails}/>
+          <Route exact = '/inventory' component = {Inventory}/>
+          <Route exact path = '/inventory/:id' component = {InventoryDetails}/>
+        </Switch>
+      </BrowserRouter>
+    </>
   );
+  }
 }
 
 export default App;
