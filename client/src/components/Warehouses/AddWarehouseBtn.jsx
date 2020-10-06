0c61e74ef62267ca2db9d3ae08dd47f29371c590
Phoebe Lo <phoebeyclo@gmail.com>
2020-10-06 13:37:18 -0700

    Warehouse List style completed

---
+++ client/src/components/Warehouses/AddWarehouseBtn.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AddWarehouseBtn extends Component {
+    render() {
+        return (
+            <div className="warehouses__add-btn btn">
+                + Add New Warehouse
+            </div>
+        )
+    }
+}
+
+export default AddWarehouseBtn;
