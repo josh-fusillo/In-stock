06fca211162a77394cccd7abd74b3ae11b93928a
Phoebe Lo <phoebeyclo@gmail.com>
2020-10-07 08:52:20 -0700

    Fix the width for card detail item

--- client/src/Components/InventoryList/InventoryCard.jsx
+++ client/src/Components/InventoryList/InventoryCard.jsx
@@ -10,30 +10,30 @@ class InventoryCard extends Component {
                 <div className="card__content-wrapper">
                     <div className="card__detail">
                         <div className="card__detail-info">
-                            <div className="card__detail-item">
+                            <div className="card__detail-item inventory__detail-item">
                                 <div className="card__detail-label">inventory item</div>
                                 <div className="card__detail-text card__detail-name inventory__info">
                                     Television <IconRight />
                                 </div>
                             </div>
-                            <div className="card__detail-item">
+                            <div className="card__detail-item inventory__detail-item">
                                 <div className="card__detail-label">category</div>
                                 <div className="card__detail-text inventory__info">Electronics</div>
                             </div>
 
-                            <div className="card__detail-item">
+                            <div className="card__detail-item inventory__detail-item">
                                 <div className="card__detail-label">status</div>
                                 <div className="card__detail-text card__detail-status inventory__info">
                                     in stock
                                 </div>
                             </div>
                             
-                            <div className="card__detail-item">
+                            <div className="card__detail-item inventory__detail-item">
                                 <div className="card__detail-label">qty</div>
                                 <div className="card__detail-text inventory__info">500</div>
                             </div>
 
-                            <div className="card__detail-item">
+                            <div className="card__detail-item inventory__detail-item">
                                 <div className="card__detail-label">warehouse</div>
                                 <div className="card__detail-text inventory__info">Manhattan</div>
                             </div>
