3f27c9d092e8e76d5f69b020fd7ecaeca08a9f30
Phoebe Lo <phoebeyclo@gmail.com>
2020-10-07 10:26:27 -0700

    Fixing the flex element for warehouses list

--- client/src/App.js
+++ client/src/App.js
@@ -8,25 +8,31 @@ import WarehouseList from './Components/WarehouseList/WarehouseList';
 import WarehouseEdit from './Components/WarehouseEdit/WarehouseEdit';
 import WarehouseAdd from './Components/WarehouseAdd/WarehouseAdd';
 
+// NEED TO DELETE
+import WarehousesListing from './Components/WarehouseList/WarehousesListing';
+
 class App extends React.Component {
   render() {
   return (
      <div className="App">
-       <BrowserRouter>
-        <Header />
+       {/* <BrowserRouter>
+        <Header /> */}
          {/* <Switch> */}
-          <Route exact path = '/warehouse/warehouseList' component = {WarehouseList}/>
+          {/* <Route exact path = '/warehouse/warehouseList' component = {WarehouseList}/>
           <Route exact path = '/warehouse/warehouseDetails' component = {WarehouseDetails}/>
           <Route exact path = '/warehouse/inventoryList' component = {InventoryList}/>
           <Route exact path = '/warehouse/warehouseEdit' component = {WarehouseEdit}/>
-          <Route exact path = '/warehouse/warehouseAdd' component = {WarehouseAdd}/> 
+          <Route exact path = '/warehouse/warehouseAdd' component = {WarehouseAdd}/>  */}
           {/* <Redirect exact from = '/' to = '/warehouse'/>
           <Route exact = '/warehouse' component {Warehouse}/> */}
           {/* <Route exact path = '/warehouse/:id' component = {WarehouseDetails}/> */}
           {/* <Route exact = '/inventory' component = {Inventory}/>
           <Route exact path = '/inventory/:id' component = {InventoryDetails}/> */}
          {/* </Switch> */}
-      </BrowserRouter>
+      {/* </BrowserRouter> */}
+
+      {/* NEE TO DELETE */}
+      <WarehousesListing />
      </div>
   );
   }
