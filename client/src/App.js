9ea04b4170ead84c33bfd05381591207447763d8
Phoebe Lo <phoebeyclo@gmail.com>
2020-10-06 17:50:41 -0700

    Editing style - minor change

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,36 @@
 import React from 'react';
-import './App.css';
-import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import Header from './Components/Header/Header'
+import './App.scss';
+// import {BrowserRouter, Switch, Route} from 'react-router-dom';
+// import Header from './Components/Header/Header'
 // import Warehouse from '';
-import WarehouseDetails from './Components/WareHouseDetails/WareHouseDetails';
-import WarehousesList from './components/Warehouses/WarehousesList.jsx';
+// import WarehouseDetails from './Components/WareHouseDetails/WareHouseDetails';
+// import WarehousesList from './components/Warehouses/WarehousesList.jsx';
 // import Inventory from '';
 // import InventoryDetails from '';
 
-import WarehouseEdit from './Components/WarehouseEdit/WarehouseEdit';
-import WarehouseAdd from './Components/WarehouseAdd/WarehouseAdd';
+// import WarehouseEdit from './Components/WarehouseEdit/WarehouseEdit';
+// import WarehouseAdd from './Components/WarehouseAdd/WarehouseAdd';
 
 class App extends React.Component {
   render() {
   return (
-    <>
+    // <>
      <div className="App">
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Header />
         <Switch>
           <Route exact path = '/warehouse/warehouseEdit' component = {WarehouseEdit}/>
-          <Route exact path = '/warehouse/warehouseAdd' component = {WarehouseAdd}/>
+          <Route exact path = '/warehouse/warehouseAdd' component = {WarehouseAdd}/> */}
           {/* <Redirect exact from = '/' to = '/warehouse'/>
           <Route exact = '/warehouse' component {Warehouse}/> */}
-          <Route exact path = '/warehouse/:id' component = {WarehouseDetails}/>
+          {/* <Route exact path = '/warehouse/:id' component = {WarehouseDetails}/> */}
           {/* <Route exact = '/inventory' component = {Inventory}/>
           <Route exact path = '/inventory/:id' component = {InventoryDetails}/> */}
-        </Switch>
-      </BrowserRouter>
+        {/* </Switch>
+      </BrowserRouter> */}
+      {/* <WarehousesList /> */}
      </div>
-    </>
+    // </>
   );
   }
 }
