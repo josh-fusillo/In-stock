71a201b4ef147d58c67924d7489b45dc890f497a
Phoebe Lo <phoebeyclo@gmail.com>
2020-10-11 10:25:30 -0700

    Clean up WarehousesList code

--- client/src/Components/Modals/WarehouseModal.jsx
+++ client/src/Components/Modals/WarehouseModal.jsx
@@ -43,82 +43,26 @@ export default class WarehouseModal extends Component {
     }
 }
 
-// import React, { Component } from 'react';
-// import Modal from 'react-awesome-modal';
-// import './Modal.scss';
-// import closeButton from '../../assets/Icons/close-24px.svg';
-
-
-// export default class WarehouseModal extends Component {
-//     constructor(props){
-//         super();
-//         this.state = {
-//             visible: false
-//         }
-//     }
-
-//     openModal = (id) => {
-//         this.setState({
-//             currentId: id,
-//             visible: true
-//         });
-//     }
-
-//     closeModal = (event, id) => {
-//         // event.preventDefault();
-//         console.log(event, id);
-//         this.setState({
-//             currentId: id,
-//             visible: false,
-//         });
-//     }
-
-//     // closeModal() {
-//     //     this.setState({
-//     //         visible: false
-//     //     });
-//     // }
-
-//     render() {
-//         return (
-//             <section>
-//                 <input type="button" className="delete__button" onClick={() => this.openModal()} />
-//                 <Modal visible={this.state.visible} effect="fadeInDown" onClickAway={() => this.closeModal()}>
-//                     <div className="delete__modal">
-//                         <h1 className="modal__header">Delete {this.props.whModel.name} warehouse?</h1>
-//                         <p className="modal__text">Please confirm that you'd like to delete the {this.props.whModel.name} from the list of warehouses.  You won't be able to undo this action.</p>
-//                         <button className="modal-cancel button" onClick={() => this.closeModal()}>Cancel</button>
-//                         {/* <button onClick={(event) => this.closeModal(event, this.props.whModel.id)} className="modal-delete button">Delete</button> */}
-//                         <button onClick={(event) => this.closeModal(event, this.props.whModel.id)} className="modal-delete button">Delete</button>
-//                         <a href="" onClick={() => this.closeModal()}><img className="close__X" src={closeButton} alt="Close Button" /></a>
-//                     </div>
-//                 </Modal>
-//             </section>
-//         )
-//     }
-// }
-
-
-//  const WarehouseModal =(
-//     visible,
-//     closeModal,
-//     openModal,
-//     props
-//  ) => {
+// const WarehouseModal = ({
+//     onDelete,
+//     closeDeleteModel,
+//     currentItemName
+// }) => {
 //     return (
-//             <section>
-//                 <input type="button" className="delete__button" onClick={ openModal } />
-//                 <Modal visible={ visible } effect="fadeInDown" onClickAway={ closeModal }>
-//                     <div className="delete__modal">
-//                         <h1 className="modal__header">Delete {props.whModel.name} warehouse?</h1>
-//                         <p className="modal__text">Please confirm that you'd like to delete the {props.whModel.name} from the list of warehouses.  You won't be able to undo this action.</p>
-//                         <button onClick={ closeModal } className="modal-cancel button">Cancel</button>
-//                         <button className="modal-delete button">Delete</button>
-//                         <a href="" onClick={ closeModal }><img className="close__X" src={closeButton} alt="Close Button" /></a>
-//                     </div>
-//                 </Modal>
-//             </section>
+//         <section>
+//             <input type="button" className="delete__button" onClick={closeDeleteModel} />
+//             <Modal effect="fadeInDown" onClickAway={closeDeleteModel}>
+//                 <div className="delete__modal">
+//                     <h1 className="modal__header">Delete {currentItemName} warehouse?</h1>
+//                     <p className="modal__text">Please confirm that you'd like to delete the {currentItemName} from the list of warehouses.  You won't be able to undo this action.</p>
+//                     <button className="modal-cancel button" onClick={closeDeleteModel}>Cancel</button>
+//                     {/* <button onClick={(event) => this.closeModal(event, this.props.whModel.id)} className="modal-delete button">Delete</button> */}
+//                     <button onClick={onDelete} className="modal-delete button">Delete</button>
+//                     <a href="" onClick={closeDeleteModel}><img className="close__X" src={closeButton} alt="Close Button" /></a>
+//                 </div>
+//             </Modal>
+//         </section>
 //     )
-// }
+// };
 
 // export default WarehouseModal;
--- client/src/Components/WarehousesList/WarehousesList.jsx
+++ client/src/Components/WarehousesList/WarehousesList.jsx
@@ -6,12 +6,14 @@ import WarehousesTitle from './WarehousesTitle';
 import IconSearch from '../Icons/IconSearch';
 import './WarehousesList.scss';
 import FilterResults from 'react-filter-search';
+import WarehouseModal from '../Modals/WarehouseModal';
 
 class WarehousesList extends Component {
     state = {
         warehousesInfo: [],
         value: '',
         currentId: null,
+        currentItemName: "",
         visible: false
     }
 
@@ -41,19 +43,21 @@ class WarehousesList extends Component {
         }
     };
 
-    // openDeleteModel = (id) => {
-    //     this.setState({
-    //         currentId: id,
-    //         visible: true
-    //     })
-    // };
+    openDeleteModel = (id, itemName) => {
+        this.setState({
+            currentId: id,
+            currentItemName: itemName,
+            visible: true
+        })
+    };
 
-    // closeDeleteModel = () => {
-    //     this.setState({
-    //         currentId: null,
-    //         visible: false
-    //     })
-    // };
+    closeDeleteModel = () => {
+        this.setState({
+            currentId: null,
+            currentItemName: "",
+            visible: false
+        })
+    };
 
     componentDidMount() {
         this.getWarehousesList();
@@ -118,11 +122,21 @@ class WarehousesList extends Component {
                                                     whData={data}
                                                     // openDeleteModel={this.openDeleteModel}
                                                     // closeDeleteModel={this.closeDeleteModel}
-                                                    handleDelete={this.handleDelete}
+                                                    openDeleteModel={this.openDeleteModel}
                                                 />
                                             ))}
                                         </div>
                                     )} />
+
+                                    {/* {this.state.value && (
+                                        <WarehouseModal
+                                            onDelete={this.handleDelete}
+                                            closeDeleteModel={this.closeDeleteModel}
+                                            currentItemName={this.state.currentItemName}
+                                        />
+                                    ) */}
+
+                                
                             </div>
                         </div>
 
@@ -136,165 +150,3 @@ class WarehousesList extends Component {
 }
 
 export default WarehousesList;
-
-// import React, { Component } from 'react';
-// import {Link} from 'react-router-dom';
-// import axios from 'axios';
-// import WarehousesCard from './WarehousesCard';
-// import WarehousesTitle from './WarehousesTitle';
-// import IconSearch from '../Icons/IconSearch';
-// import './WarehousesList.scss';
-// import FilterResults from 'react-filter-search';
-
-// class WarehousesList extends Component {
-//     state = {
-//         warehousesInfo: [],
-//         value: '',
-//         currentId: null
-//     }
-
-//     getWarehousesList = () => {
-//         axios
-//             .get(`http://localhost:8080/warehouses/`)
-//             .then(res => {
-//                         this.setState({                        
-//                         warehousesInfo: res.data
-//                     })   
-//                     console.log(this.state.warehousesInfo)
-
-//             })                  
-//     }   
-    
-//     onRemoveModel = () => {
-//         this.setState({
-//             currentId: null
-//         })
-//     }
-    
-//     // handleRemove = () => {
-//     //     const id = this.state.currentId;
-//     //     axios
-//     //         .delete(`http://localhost:8080/warehouses/${id}`)
-//     //         .then(res => {
-//     //             console.log(res);
-//     //             this.onRemoveModel();
-//     //     })
-//     // }
-
-//     handleRemove = async () => {
-//         try{
-//             const id = this.state.currentId;
-//             await axios.delete(`http://localhost:8080/warehouses/${id}`);
-//             this.getWarehousesList();
-//             this.onRemoveModel();
-//         } catch (err) {
-//             console.log(err);
-//         }
-//     }
-
-//     componentDidMount() {
-//         this.getWarehousesList();    
-//         console.log(this.state.warehousesInfo)           
-//     }
-
-//     componentDidUpdate() {
-//         this.handleRemove();
-//     }
-
-
-//     handleChange = event => {
-//         const { value } = event.target;
-//         this.setState({ value });
-//     };
-
-
-//     render() {
-//         const { warehousesInfo, value } = this.state;
-//         return (
-//             <div className="container">
-//                 <div className="warehouses">
-//                     <div className="warehouses__card-wrapper wrapper">
-//                         <div className="warehouses__card-content">
-//                             <div className="warehouses__header-container">
-//                                 <div className="warehouses__header-left">Warehouses</div>
-
-//                                 <div className="warehouses__header-right">
-//                                     <div className="warehouses__search-container input">
-//                                         <input type="text"
-//                                             value={value}
-//                                             onChange={this.handleChange}
-//                                             placeholder="Search ..."
-//                                             className="warehouses__search-input" />
-//                                         <IconSearch />
-//                                     </div>
-
-//                                     <Link to="/warehouse/warehouseAdd">
-//                                         <div className="warehouses__add-btn btn-large">
-//                                             + Add New Warehouse
-//                                         </div>
-//                                     </Link>
-
-//                                 </div>
-//                             </div>
-
-//                             <WarehousesTitle />
-
-//                             <FilterResults
-//                                 value={value}
-//                                 data={warehousesInfo}
-//                                 renderResults={results => (
-//                                     <div>
-//                                         {results.map(data => (
-//                                             <WarehousesCard
-//                                                 key={data.id}
-//                                                 id={data.id}
-//                                                 warehouseId={data.id}
-//                                                 name={data.name}
-//                                                 address={data.address}
-//                                                 city={data.city}
-//                                                 country={data.country}
-//                                                 contactname={data.contact.name}
-//                                                 phone={data.contact.phone}
-//                                                 email={data.contact.email}
-//                                             />
-//                                         ))}
-//                                     </div>
-//                                 )} />
-//                         </div>
-//                     </div>
-
-//                     {/* // {this.state.warehousesInfo.map(data => 
-//                     // <WarehousesCard 
-//                     //     key={data.id} 
-//                     //     warehouseId={data.id}
-//                     //     name={data.name} 
-//                     //     address={data.address} 
-//                     //     city={data.city} 
-//                     //     country={data.country} 
-//                     //     contactname={data.contact.name}
-//                     //     phone={data.contact.phone}
-//                     //     email={data.contact.email}        
-//                     //  />)} */}
-
-//                      {/* {this.state.warehousesInfo.map(data =>  */}
-//                      {/* <WarehousesCard 
-//                         // key={data.id} 
-//                         // warehouseId={data.id}
-//                         // name={data.name} 
-//                         // address={data.address} 
-//                         // city={data.city} 
-//                         // country={data.country} 
-//                          // contactname={data.contact.name}
-//                          // phone={data.contact.phone}
-//                          // email={data.contact.email} 
-//                          whData = {data}
-//                          onRemoveWh = {this.handleRemove} 
-//                          onRemoveModel = {this.onRemoveModel}  
-//                       />)} */}
-//                 </div>
-//             </div>
-//         )
-//     }
-// }
-
-// export default WarehousesList;
